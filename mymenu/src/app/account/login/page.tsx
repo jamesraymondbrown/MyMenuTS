a4d432997ff024e963044b61e75e185bc1326bba
James Brown <jamesraymondbrown@gmail.com>
2024-01-18 14:46:32 -0800

    update gitignore

---
+++ mymenu/src/app/account/login/page.tsx
@@ -0,0 +1,12 @@
+const LoginPage = () => {
+  return (
+    // <form action={authenticate}>
+    //   <input type="email" name="email" placeholder="Email" required />
+    //   <input type="password" name="password" placeholder="Password" required />
+    //   <button type="submit">Login</button>
+    // </form>
+    <p>Snail</p>
+  );
+};
+
+export default LoginPage;
